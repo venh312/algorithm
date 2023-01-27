c1bdca6a445fbc961259bb7062ec56e9130650eb
VenhMac <gpfm312@gmail.com>
2023-01-27 21:15:45 +0900

    [add] Q_2839

---
+++ src/main/java/greedy/Q2839/Main.java
@@ -0,0 +1,23 @@
+package greedy.Q2839;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int cnt = 0;
+
+        while (true) {
+            if (n % 5 == 0) {
+                System.out.println(n/5 + cnt);
+                break;
+            } else if (n < 0) {
+                System.out.println(-1);
+                break;
+            }
+            n -= 3;
+            cnt++;
+        }
+    }
+}
--- src/main/java/greedy/Q_2839.java
+++
@@ -1,9 +0,0 @@
-package greedy;
-
-import java.util.Scanner;
-
-public class Q_2839 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-    }
-}
