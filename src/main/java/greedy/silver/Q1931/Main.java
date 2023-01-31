59ee4352410eac0d52068537428b5cf807a017d0
venh <conf312@naver.com>
2023-01-31 19:10:55 +0900

    [add] Q_1026, Q_1931

--- src/main/java/greedy/silver/Q1931/Main.java
+++ src/main/java/greedy/silver/Q1931/Main.java
@@ -15,18 +15,21 @@ public class Main {
             arr[i][1] = Integer.parseInt(s[1]);
         }
 
-        Arrays.sort(arr, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                return o1[0]!=o2[0] ? o1[0]-o2[0] : o1[1]-o2[1]; // 첫번째 기준 오름차순 > 두번째 기준 오름차순
-            }
+        Arrays.sort(arr, (o1, o2) -> {
+            if(o1[1] == o2[1]) // 종료시간이 같을 경우 시작 시간이 빠른 순
+                return o1[0] - o2[0];
+            return o1[1] - o2[1];
         });
 
+        int cnt = 0;
+        int end = 0;
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < 2; j++) {
-                System.out.print(arr[i][j]);
+            if(end <= arr[i][0]) {
+                end = arr[i][1];
+                cnt++;
             }
-            System.out.println();
         }
+
+        System.out.println(cnt);
     }
 }
