c4cd8ff1c361c4fb3a68ac90351cf2f6c251b175
venh <conf312@naver.com>
2023-03-12 22:42:42 +0900

    [add] Q_2563

---
+++ src/main/java/Implementation/silver/Q2563/Main.java
@@ -0,0 +1,31 @@
+package Implementation.silver.Q2563;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int answer = 0;
+        int n = Integer.parseInt(br.readLine());
+        boolean[][] arr = new boolean[101][101];
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            for (int j = x; j < x+10; j++) {
+                for (int k = y; k < y+10; k++) {
+                    if (!arr[j][k]) {
+                        arr[j][k] = true;
+                        answer++;
+                    }
+                }
+            }
+        }
+        bw.write(answer + "");
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
