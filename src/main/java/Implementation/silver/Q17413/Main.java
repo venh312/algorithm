47c98212b2c9493e8b934c28b10bed78505ecee4
venh <conf312@naver.com>
2023-03-16 21:22:28 +0900

    [add] Q_17413

--- src/main/java/Implementation/silver/Q17413/Main.java
+++ src/main/java/Implementation/silver/Q17413/Main.java
@@ -1,2 +1,40 @@
-package Implementation.silver.Q17413;public class Main {
+package Implementation.silver.Q17413;
+
+import java.io.*;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String input = br.readLine();
+        Stack<Character> stack = new Stack<>();
+        boolean flag = false;
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == '<') {
+                flag = true;
+                while (!stack.isEmpty())
+                    bw.write(stack.pop());
+                bw.write(input.charAt(i));
+            } else if (input.charAt(i) == '>') {
+                flag = false;
+                bw.write(input.charAt(i));
+            } else if (flag) {
+                bw.write(input.charAt(i));
+            } else {
+                if (input.charAt(i) == ' ') {
+                    while (!stack.isEmpty())
+                        bw.write(stack.pop());
+                    bw.write(" ");
+                } else {
+                    stack.push(input.charAt(i));
+                }
+            }
+        }
+        while (!stack.isEmpty())
+            bw.write(stack.pop());
+        bw.flush();
+        br.close();
+        bw.close();
+    }
 }
