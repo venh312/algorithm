51acecdcc7b0af503294feb80700909e2810b739
conf312 <conf312@naver.com>
2023-08-07 10:26:45 +0900

    문자열 Q1152

---
+++ src/main/java/etc/Q1152/Main.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.Arrays;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String word = br.readLine().trim();
+        if (word.isEmpty()) {
+            bw.write("0");
+        } else {
+            String[] wordArr = word.trim().split(" ");
+            bw.write(Arrays.stream(wordArr).count() + "");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
