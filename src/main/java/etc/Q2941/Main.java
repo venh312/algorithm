41b9048565279c628a8543af4c59e09b7bfacb73
conf312 <conf312@naver.com>
2023-08-07 13:02:19 +0900

    심화1 Q2941

---
+++ src/main/java/etc/Q2941/Main.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String input = br.readLine();
+
+        String[] alphabet = {"c=","c-","dz=","d-","lj","nj","s=","z="};
+
+        for (String s : alphabet)
+            input = input.replace(s, "#");
+
+        int count = 0;
+        for (int i = 0; i < input.length(); i++)
+            if (input.charAt(i) == '#') ++count;
+        
+        input = input.replace("#", "");
+        bw.write((count + input.length()) + "");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
