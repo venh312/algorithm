26aa190670ff4c436742aad3ec165d98f79c5a90
conf312 <conf312@naver.com>
2023-10-10 14:40:24 +0900

    동적계획법 Q1932

---
+++ src/main/java/dp/Q1932/Main.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] arr = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            String[] splitArr =  br.readLine().split(" ");
+            for (int j = 0; j < splitArr.length; j++)
+                arr[i][j] = Integer.parseInt(splitArr[j]);
+        }
+
+        int[][] dp = new int[n][n];
+        dp[0][0] = arr[0][0];
+
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = dp[i - 1][0] + arr[i][0];
+            dp[i][i] = dp[i - 1][i - 1] + arr[i][i];
+
+            for (int j = 1; j < i; j++)
+                dp[i][j] = Math.max(dp[i - 1][j - 1], dp[i - 1][j]) + arr[i][j];
+        }
+
+        int maxSum = dp[n - 1][0];
+        for (int i = 1; i < n; i++)
+            maxSum = Math.max(maxSum, dp[n - 1][i]);
+
+        System.out.println(maxSum);
+    }
+}
