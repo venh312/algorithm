e066072a6dc69ccd8cfa53d05b5f6508eb149eb5
conf312 <conf312@naver.com>
2023-10-19 11:15:29 +0900

    동적계획법 Q1463

---
+++ src/main/java/dp/Q1463/Main.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n + 1];
+      
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + 1;
+
+            if (i % 2 == 0)
+                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+
+            if (i % 3 == 0)
+                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+        }
+
+        System.out.println(dp[n]);
+    }
+}
