4b3ce2fac34be42d09069fa4a2ea21c4c9abf433
conf312 <conf312@naver.com>
2023-11-02 08:48:30 +0900

    동적계획법 Q11053

---
+++ src/main/java/dp/Q11053/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        int[] dp = new int[n];
+        int maxLength = 1;
+
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j] && dp[i] < dp[j] + 1) {
+                    dp[i] = dp[j] + 1;
+                }
+            }
+            maxLength = Math.max(maxLength, dp[i]);
+        }
+
+        System.out.println(maxLength);
+    }
+}
