df44a1afbc24385a14d5fb4ab1637b2d16162a6c
conf312 <conf312@naver.com>
2024-04-02 11:06:26 +0900

    Create LengthOfLastWord.java

---
+++ src/main/java/leetcode/problems/LengthOfLastWord.java
@@ -0,0 +1,16 @@
+/*
+Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+A word is a maximal substring consisting of non-space characters only.
+
+Example 1:
+Input: s = "Hello World"
+Output: 5
+Explanation: The last word is "World" with length 5.
+*/
+public class LengthOfLastWord {
+  public int lengthOfLastWord(String s) {
+    String[] sArr = s.split(" ");
+    return sArr[sArr.length-1].length();
+  }
+}
