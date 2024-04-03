df3d6cc170d7f7a273cd8332c2f5d78c30bf885d
conf312 <conf312@naver.com>
2024-04-03 14:56:56 +0900

    Update TwoSum.java

--- src/main/java/leetcode/problems/TwoSum.java
+++ src/main/java/leetcode/problems/TwoSum.java
@@ -1,3 +1,4 @@
+package leetcode.problems;
 /*
 Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
@@ -10,7 +11,7 @@ Input: nums = [2,7,11,15], target = 9
 Output: [0,1]
 Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 */
-class TwoSum {
+public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> hMap = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
