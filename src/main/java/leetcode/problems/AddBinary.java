bb3985601f8cbbca1a191601f4389df098e415e8
conf312 <conf312@naver.com>
2024-04-03 14:56:16 +0900

    Update AddBinary.java

--- src/main/java/leetcode/problems/AddBinary.java
+++ src/main/java/leetcode/problems/AddBinary.java
@@ -1,5 +1,16 @@
 package leetcode.problems;
 
+/*
+Given two binary strings a and b, return their sum as a binary string.
+
+Example 1:
+Input: a = "11", b = "1"
+Output: "100"
+
+Example 2:
+Input: a = "1010", b = "1011"
+Output: "10101"
+*/
 public class AddBinary {
   public String addBinary(String a, String b) {
       StringBuilder sb = new StringBuilder();
