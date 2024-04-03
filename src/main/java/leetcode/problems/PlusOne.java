7d2697db51b5e018ea21c2dd1b853b3fd71ceeea
conf312 <conf312@naver.com>
2024-04-03 14:56:42 +0900

    Update PlusOne.java

--- src/main/java/leetcode/problems/PlusOne.java
+++ src/main/java/leetcode/problems/PlusOne.java
@@ -1,3 +1,4 @@
+package leetcode.problems;
 /*
 You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
 
@@ -10,7 +11,7 @@ Explanation: The array represents the integer 123.
 Incrementing by one gives 123 + 1 = 124.
 Thus, the result should be [1,2,4].
 */
-class PlusOne {
+public class PlusOne {
     public int[] plusOne(int[] digits) {
         return restruct(digits);
     }
