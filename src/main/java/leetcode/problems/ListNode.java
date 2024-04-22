781bee9d355a92495a3419db3e1d969c324526e6
conf312 <conf312@naver.com>
2024-04-22 10:37:03 +0900

    Create ListNode.java

---
+++ src/main/java/leetcode/problems/ListNode.java
@@ -0,0 +1,10 @@
+package leetcode.problems;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    public ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
