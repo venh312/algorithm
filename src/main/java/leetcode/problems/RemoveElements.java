597dc8ece36eb9b92527912f6b4151dac8b79de6
conf312 <conf312@naver.com>
2024-04-29 10:49:58 +0900

    Create RemoveElements.java
    203. Remove Linked List Elements

---
+++ src/main/java/leetcode/problems/RemoveElements.java
@@ -0,0 +1,45 @@
+/**
+Given the head of a linked list and an integer val, 
+remove all the nodes of the linked list that has Node.val == val, 
+and return the new head.
+
+Example 1:
+Input: head = [1,2,6,3,4,5,6], val = 6
+Output: [1,2,3,4,5]
+
+Example 2:
+Input: head = [], val = 1
+Output: []
+
+Example 3:
+Input: head = [7,7,7,7], val = 7
+Output: []
+
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class RemoveElements {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+        ListNode curr = head;
+
+        while (curr != null) {
+            if (curr.val == val) {
+                prev.next = curr.next;
+            } else {
+                prev = curr;
+            }
+            curr = curr.next;
+        }
+
+        return dummy.next;
+    }
+}
