7601ee3bb6c1d2f0693c31bfc35f920bbfbaa43b
venh <conf312@naver.com>
2024-05-18 13:06:15 +0900

    Create NumArray.java

---
+++ src/main/java/leetcode/problems/NumArray.java
@@ -0,0 +1,40 @@
+package leetcode.problems;
+
+/*
+Given an integer array nums, handle multiple queries of the following type:
+
+Calculate the sum of the elements of nums between indices left and right inclusive where left <= right.
+Implement the NumArray class:
+
+NumArray(int[] nums) Initializes the object with the integer array nums.
+int sumRange(int left, int right) Returns the sum of the elements of nums between indices left and right inclusive (i.e. nums[left] + nums[left + 1] + ... + nums[right]).
+ 
+Example 1:
+Input
+["NumArray", "sumRange", "sumRange", "sumRange"]
+[[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
+Output
+[null, 1, -1, -3]
+
+Explanation
+NumArray numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
+numArray.sumRange(0, 2); // return (-2) + 0 + 3 = 1
+numArray.sumRange(2, 5); // return 3 + (-5) + 2 + (-1) = -1
+numArray.sumRange(0, 5); // return (-2) + 0 + 3 + (-5) + 2 + (-1) = -3
+*/
+public class NumArray {
+    private int[] prefixSums;
+
+    public NumArray(int[] nums) {
+        int n = nums.length;
+        prefixSums = new int[n + 1];
+        // prefixSums[i]는 nums의 첫 번째 i개의 원소의 합
+        for (int i = 0; i < n; i++) {
+            prefixSums[i + 1] = prefixSums[i] + nums[i];
+        }
+    }
+
+    public int sumRange(int left, int right) {
+        return prefixSums[right + 1] - prefixSums[left];
+    }
+}
