ff399d50e97d5fcb95b50d50c99821c0fab2bc84
venh <conf312@naver.com>
2024-05-23 13:27:50 +0900

    Create GuessNumber.java
    374. Guess Number Higher or Lower

---
+++ src/main/java/leetcode/problems/GuessNumber.java
@@ -0,0 +1,47 @@
+/*
+We are playing the Guess Game. The game is as follows:
+
+I pick a number from 1 to n. You have to guess which number I picked.
+
+Every time you guess wrong, I will tell you whether the number I picked is higher or lower than your guess.
+
+You call a pre-defined API int guess(int num), which returns three possible results:
+
+-1: Your guess is higher than the number I picked (i.e. num > pick).
+1: Your guess is lower than the number I picked (i.e. num < pick).
+0: your guess is equal to the number I picked (i.e. num == pick).
+Return the number that I picked.
+
+Example 1:
+Input: n = 10, pick = 6
+Output: 6
+
+Example 2:
+Input: n = 1, pick = 1
+Output: 1
+
+Example 3:
+Input: n = 2, pick = 1
+Output: 1
+*/
+public class GuessNumber extends GuessGame {
+    public int guessNumber(int n) {
+        int left = 1;
+        int right = n;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int result = guess(mid);
+            
+            if (result == 0) {
+                return mid;
+            } else if (result == -1) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        
+        return -1;
+    }
+}
