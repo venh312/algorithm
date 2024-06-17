315954f3c82d5675101818bf9489f5d5ed398f0d
venh <conf312@naver.com>
2024-06-17 14:09:46 +0900

    Create IsSubtree.java
    572. Subtree of Another Tree

---
+++ src/main/java/leetcode/problems/IsSubtree.java
@@ -0,0 +1,39 @@
+package leetcode.problems;
+
+/*
+Given the roots of two binary trees root and subRoot, return true if there is a subtree of root with the same structure and node values of subRoot and false otherwise.
+A subtree of a binary tree tree is a tree that consists of a node in tree and all of this node's descendants. The tree tree could also be considered as a subtree of itself.
+
+Example 1:
+Input: root = [3,4,5,1,2], subRoot = [4,1,2]
+Output: true
+
+Example 2:
+Input: root = [3,4,5,1,2,null,null,null,null,0], subRoot = [4,1,2]
+Output: false
+*/
+class IsSubtree {
+    // Runtime: 2 ms, faster than 96.51%
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (root == null) {
+            return false;
+        }
+        if (isSame(root, subRoot)) {
+            return true;
+        }
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+
+    private boolean isSame(TreeNode root1, TreeNode root2) {
+        if (root1 == null && root2 == null) {
+            return true;
+        }
+        if (root1 == null || root2 == null) {
+            return false;
+        }
+        if (root1.val != root2.val) {
+            return false;
+        }
+        return isSame(root1.left, root2.left) && isSame(root1.right, root2.right);
+    }
+}
