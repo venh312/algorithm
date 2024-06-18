45bc271df76c510f7961dd962dbe0233156fa3a5
venh <conf312@naver.com>
2024-06-18 16:48:56 +0900

    Create MaxCount.java
    598. Range Addition II

---
+++ src/main/java/leetcode/problems/MaxCount.java
@@ -0,0 +1,37 @@
+package leetcode.problems;
+
+/*
+You are given an m x n matrix M initialized with all 0's and an array of operations ops, where ops[i] = [ai, bi] means M[x][y] should be incremented by one for all 0 <= x < ai and 0 <= y < bi.
+Count and return the number of maximum integers in the matrix after performing all the operations.
+
+Example 1:
+Input: m = 3, n = 3, ops = [[2,2],[3,3]]
+Output: 4
+Explanation: The maximum integer in M is 2, and there are four of it in M. So return 4.
+
+Example 2:
+Input: m = 3, n = 3, ops = [[2,2],[3,3],[3,3],[3,3],[2,2],[3,3],[3,3],[3,3],[2,2],[3,3],[3,3],[3,3]]
+Output: 4
+
+Example 3:
+Input: m = 3, n = 3, ops = []
+Output: 9
+*/
+class MaxCount {
+    // Runtime: 0 ms, faster than 100.00%
+    public int maxCount(int m, int n, int[][] ops) {
+        if (ops == null || ops.length == 0) {
+            return m * n; // If ops is empty, the matrix remains all 0's
+        }
+
+        int minRow = m;
+        int minCol = n;
+
+        for (int[] op : ops) {
+            minRow = Math.min(minRow, op[0]);
+            minCol = Math.min(minCol, op[1]);
+        }
+
+        return minRow * minCol;
+    }
+}
