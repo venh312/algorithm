33d4a0ed5b4b8f9b1df31e80b35b63f17f7d89a7
venh <conf312@naver.com>
2024-07-01 13:20:41 +0900

    Create SearchBST.java
    700. Search in a Binary Search Tree

---
+++ src/main/java/leetcode/problems/SearchBST.java
@@ -0,0 +1,28 @@
+package leetcode.problems;
+
+/*
+You are given the root of a binary search tree (BST) and an integer val.
+
+Find the node in the BST that the node's value equals val and return the subtree rooted with that node. If such a node does not exist, return null.
+
+Example 1:
+Input: root = [4,2,7,1,3], val = 2
+Output: [2,1,3]
+
+Example 2:
+Input: root = [4,2,7,1,3], val = 5
+Output: []
+*/
+class SearchBST {
+   // Runtime: 0 ms, faster than 100.00%
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) return null;
+        if (root.val == val) {
+            return root;
+        } else if (root.val > val) {
+            return searchBST(root.left, val);
+        }  else {
+            return searchBST(root.right, val);
+        }
+    }
+}
