5bfe687d4c567155aea1337e3e4c779241071f48
venh <conf312@naver.com>
2024-07-10 18:56:58 +0900

    Create CountPrimeSetBits.java
    762. Prime Number of Set Bits in Binary Representation

---
+++ src/main/java/leetcode/problems/CountPrimeSetBits.java
@@ -0,0 +1,65 @@
+package leetcode.problems;
+
+/*
+Given two integers left and right, return the count of numbers in the inclusive range [left, right] having a prime number of set bits in their binary representation.
+
+Recall that the number of set bits an integer has is the number of 1's present when written in binary.
+
+For example, 21 written in binary is 10101, which has 3 set bits.
+ 
+Example 1:
+Input: left = 6, right = 10
+Output: 4
+Explanation:
+6  -> 110 (2 set bits, 2 is prime)
+7  -> 111 (3 set bits, 3 is prime)
+8  -> 1000 (1 set bit, 1 is not prime)
+9  -> 1001 (2 set bits, 2 is prime)
+10 -> 1010 (2 set bits, 2 is prime)
+4 numbers have a prime number of set bits.
+
+Example 2:
+Input: left = 10, right = 15
+Output: 5
+Explanation:
+10 -> 1010 (2 set bits, 2 is prime)
+11 -> 1011 (3 set bits, 3 is prime)
+12 -> 1100 (2 set bits, 2 is prime)
+13 -> 1101 (3 set bits, 3 is prime)
+14 -> 1110 (3 set bits, 3 is prime)
+15 -> 1111 (4 set bits, 4 is not prime)
+5 numbers have a prime number of set bits.
+*/
+class CountPrimeSetBits {
+    // Runtime: 4 ms, faster than 91.97% 
+    public static int countPrimeSetBits(int left, int right) {
+        int count = 0;
+        for (int num = left; num <= right; num++) {
+            int setBits = Integer.bitCount(num);
+            if (isPrime(setBits)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    // Function to check if a number is prime
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        if (num <= 3) {
+            return true;
+        }
+        if (num % 2 == 0 || num % 3 == 0) {
+            return false;
+        }
+        for (int i = 5; i * i <= num; i += 6) {
+            if (num % i == 0 || num % (i + 2) == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
