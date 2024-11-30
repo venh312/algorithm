c421aacfaba53830c78c99e866f1e2c008e38e0d
venh <conf312@naver.com>
2024-11-30 19:33:14 +0900

    Create ZigzagLevelOrder.java

---
+++ src/main/java/leetcode/problems/ZigzagLevelOrder.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class ZigzagLevelOrder {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) return result;
+
+        // 그래프탐 > bfs 너비우선탐색, dfs 깊이우선탐색 =>
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        boolean isLeft = false;
+
+        // BFS
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (isLeft) {
+                    list.add(0, node.val);
+                } else {
+                    list.add(node.val);
+                }
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
+            isLeft = !isLeft;
+
+            result.add(list);
+        }
+
+        return result;
+    }
+}
