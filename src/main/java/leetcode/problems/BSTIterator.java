1053e0d2213a19e6ced0677eca1245898f4f17b0
venh <conf312@naver.com>
2025-01-07 10:56:31 +0900

    Create BSTIterator.java

---
+++ src/main/java/leetcode/problems/BSTIterator.java
@@ -0,0 +1,56 @@
+/*
+https://leetcode.com/problems/binary-search-tree-iterator/description/
+
+Implement the BSTIterator class that represents an iterator over the in-order traversal of a binary search tree (BST):
+
+BSTIterator(TreeNode root) Initializes an object of the BSTIterator class. The root of the BST is given as part of the constructor. The pointer should be initialized to a non-existent number smaller than any element in the BST.
+boolean hasNext() Returns true if there exists a number in the traversal to the right of the pointer, otherwise returns false.
+int next() Moves the pointer to the right, then returns the number at the pointer.
+Notice that by initializing the pointer to a non-existent smallest number, the first call to next() will return the smallest element in the BST.
+
+You may assume that next() calls will always be valid. That is, there will be at least a next number in the in-order traversal when next() is called.
+
+Example 1:
+Input
+["BSTIterator", "next", "next", "hasNext", "next", "hasNext", "next", "hasNext", "next", "hasNext"]
+[[[7, 3, 15, null, null, 9, 20]], [], [], [], [], [], [], [], [], []]
+Output
+[null, 3, 7, true, 9, true, 15, true, 20, false]
+
+Explanation
+BSTIterator bSTIterator = new BSTIterator([7, 3, 15, null, null, 9, 20]);
+bSTIterator.next();    // return 3
+bSTIterator.next();    // return 7
+bSTIterator.hasNext(); // return True
+bSTIterator.next();    // return 9
+bSTIterator.hasNext(); // return True
+bSTIterator.next();    // return 15
+bSTIterator.hasNext(); // return True
+bSTIterator.next();    // return 20
+bSTIterator.hasNext(); // return False
+*/
+class BSTIterator {
+    private List<Integer> list;
+    private int index;
+
+    public BSTIterator(TreeNode root) {
+        this.list = new ArrayList<>();
+        this.index = -1;
+        inOrder(root); // 중위 순회 결과를 리스트에 저장
+    }
+
+    private void inOrder(TreeNode root) {
+        if (root == null) return;
+        inOrder(root.left);
+        list.add(root.val);
+        inOrder(root.right);
+    }
+
+    public int next() {
+        return list.get(++index); // 인덱스를 증가시키고 해당 값 반환
+    }
+
+    public boolean hasNext() {
+        return index + 1 < list.size(); // 다음 값이 있는지 확인
+    }
+}
